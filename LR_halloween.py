173bd8ae46f1b5712e08f429d935964cb2d86c0b
Kal <kalagodk@mail.uc.edu>
2020-10-20 08:32:42 -0400

    First commit for hue halloween porch and living room lights

---
+++ LR_halloween.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on MON Oct 19 2020
+
+@author: kal @ Dilip Kalagolta (kalagodk@mail.uc.edu)
+
+Add code summary below:
+    Code to flicker hue lights controlled with
+    phue https://github.com/studioimaginaire/phue
+"""
+
+from phue import Bridge
+import time
+import random
+import sys
+from multiprocessing import Process
+import os
+
+def flicker(l_id):
+
+    b = Bridge('192.168.1.35') #Setup bridge instance
+    b.connect() #Connect to the bridge
+
+    command_off = {'on': False}
+    
+    start_time = time.time()
+    while True:
+        i = 0
+        while i<=50:
+            command_on =  {'transitiontime': random.randint(1,35), 'on': True,
+                           'bri': random.randint(150,250),
+                           'xy': [random.random(), random.random()]}
+            b.set_light(l_id, command_on)
+            b.set_light(l_id, command_off)
+            i += 1
+        i = 0
+        while i<=random.randint(1,50):
+            command_on =  {'transitiontime': 1, 'on': True,
+                           'bri': 255, 'ct': 0}
+            b.set_light(l_id, command_on)
+            b.set_light(l_id, command_off)
+            i+=1
+    end_time = time.time()
+    
+    print('Time taken= ', end_time - start_time)
+
+    return
+
+flicker([5,6,8,9])
+# def run_cpu_tasks_in_parallel(tasks):
+#     running_tasks = [Process(target=task) for task in tasks]
+#     for running_task in running_tasks:
+#         running_task.start()
+#     for running_task in running_tasks:
+#         running_task.join()
+
+# run_cpu_tasks_in_parallel([flicker(5), flicker(6), flicker(8), flicker(9)])
